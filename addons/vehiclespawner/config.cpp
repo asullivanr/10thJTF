89559a22bad5a8021f69339300db402db13926c8
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2025-05-06 03:54:05 -0500

    Update config.cpp

--- addons/vehiclespawner/config.cpp
+++ addons/vehiclespawner/config.cpp
@@ -3,7 +3,8 @@ class CfgPatches {
         author = "Akira";
         name = "10th JTF: vehicle spawner";
         units[] = {
-            "Tenthed_Spawner_VehicleSpawner"
+            "Tenthed_Spawner_VehicleSpawner",
+            "Tenthed_Spawner_VehicleSpawnerArray"
         };
         weapons[] = {};
         requiredAddons[] = {"A3_Modules_F","A3_Functions_F"};
@@ -14,20 +15,21 @@ class CfgFunctions {
     class Tenthed_functions_spawning
     {
         tag = "Tenthed_Spawner";
-        class Tenthed_Func_spawnVehicle
-        {
+        class Tenthed_Func_spawnVehicle {
             file="z\10thJTF\addons\vehiclespawner\functions";
             class spawnVehicle {};
+        };
+        class Tenthed_Func_spawnVehicleArray {
+            file="z\10thJTF\addons\vehiclespawner\functions";
+            class spawnVehicleArray {};
         };
     };
 };
 
 class CfgVehicles {
 	class Logic;
-	class Module_F : Logic
-	{
-		class AttributesBase
-		{
+	class Module_F : Logic {
+		class AttributesBase {
 			class Default;
 			class Edit;					// Default edit box (i.e. text input field)
 			class Combo;				// Default combo box (i.e. drop-down menu)
@@ -38,8 +40,7 @@ class CfgVehicles {
 		};
 
 		// Description base classes (for more information see below):
-		class ModuleDescription
-		{
+		class ModuleDescription {
             description = "Base module description.";
             class Anything;
 			class AnyBrain;
@@ -52,7 +53,7 @@ class CfgVehicles {
         displayName = "Spawn Vehicle";
         category = "Tenthed_UnitFact_Main";
 
-        function = "Tenthed_Spawner_fnc_spawnVehicle"; // called when module is placed
+        function = "Tenthed_Spawner_fnc_spawnVehicle";
         functionPriority = 1;
         isGlobal = 1;
         isTriggerActivated = 0;
@@ -70,6 +71,26 @@ class CfgVehicles {
                 description = "Comma-separated display names matching class names.";
                 typeName = "STRING";
                 defaultValue = "M2A2ODS (BUSK I),M1123 (2D/Half/Snorkel)";
+            };
+        };
+    };
+    class Tenthed_Spawner_VehicleSpawnerArray : Module_F {
+        scope = 2;
+        displayName = "Spawn Vehicle Array";
+        category = "Tenthed_UnitFact_Main";
+
+        function = "Tenthed_Spawner_fnc_spawnVehicleArray";
+        functionPriority = 1;
+        isGlobal = 1;
+        isTriggerActivated = 0;
+        isDisposable = 0;
+
+        class Arguments {
+            class vehicleEntries {
+                displayName = "Vehicles";
+                description = "List of vehicles in format: DisplayName|ClassName;DisplayName2|ClassName2";
+                typeName = "STRING";
+                defaultValue = "M2A2ODS (BUSK I)|RHS_M2A2_BUSKI;M1123 (2D/Half/Snorkel)|rhsusf_m998_d_s_2dr_halftop";
             };
         };
     };
