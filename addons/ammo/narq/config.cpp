e3b0e7ef9e774f6d9c7e470668c735280cceafdb
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2025-05-08 17:37:32 -0500

    Update config.cpp

--- addons/ammo/narq/config.cpp
+++ addons/ammo/narq/config.cpp
@@ -14,34 +14,25 @@ class CfgPatches {
 		};
 	};
 };
-class Extended_PreStart_EventHandlers
-{
-	class Tenthed_functions_narq
-	{
+class Extended_PreStart_EventHandlers {
+	class Tenthed_functions_narq {
 		init="call compile preprocessFileLineNumbers 'z\10thJTF\addons\ammo\narq\XEH_preStart.sqf'; ";
 	};
 };
-class Extended_PreInit_EventHandlers
-{
-	class Tenthed_functions_narq
-	{
+class Extended_PreInit_EventHandlers {
+	class Tenthed_functions_narq {
 		init="call compile preprocessFileLineNumbers 'z\10thJTF\addons\ammo\narq\XEH_preInit.sqf'; ";
 	};
 };
-class Extended_Respawn_EventHandlers
-{
-	class CAManBase
-	{
-		class Tenthed_functions_narq
-		{
+class Extended_Respawn_EventHandlers {
+	class CAManBase {
+		class Tenthed_functions_narq {
 			respawn="_this call CSW_fnc_NarqOnRespawn";
 		};
 	};
 };
-class Extended_Hit_EventHandlers
-{
-	class CAManBase
-	{
+class Extended_Hit_EventHandlers {
+	class CAManBase {
 		Tenthed_Narq="_this call CSW_fnc_NarqOnHit;";
 	};
 };
