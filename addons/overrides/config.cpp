f08f56e9f6436d56c0eb30ef64bab4a5f482101a
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2025-09-25 21:54:34 -0500

    override faction mod

--- addons/overrides/config.cpp
+++ addons/overrides/config.cpp
@@ -1,51 +1,105 @@
 class CfgPatches {
     class Tenthed_overrides {
         author = "Akira";
-        name = "10th JTF: Overrides";
+        name   = "10th JTF: Overrides";
         requiredAddons[] = {
-            "Tenthed_core",
             "GX_Blanco_Cartel"
         };
-        weapons[] = {};
-        units[] = {};
+        weapons[] = {
+			"TEN_Ballistic_Mask",
+			"TEN_B_M_Good",
+			"TEN_B_M_Extreme",
+			"TEN_B_M_God",
+			"TEN_B_M_Officer",
+			"TEN_B_M_SL",
+			"TEN_B_M_UP",
+			"TEN_BC_Carrier_GL_Black",
+			"TEN_BC_Carrier_Special_Black",
+			"TEN_arifle_CTARS_blk_F",
+			"TEN_srifle_DMR_04_F"
+        };
+        units[]   = {
+            "TEN_B_C_Rifleman",
+			"TEN_B_C_Medic",
+			"TEN_B_C_Engineer",
+			"TEN_B_C_MG",
+			"TEN_B_C_AT",
+			"TEN_B_C_Marksman",
+			"TEN_B_C_Officer",
+			"TEN_B_C_SL",
+			"TEN_B_C_Rifleman_Light",
+			"TEN_B_C_Medic_Light",
+			"TEN_B_C_Engineer_Light",
+			"TEN_B_C_MG_Light",
+			"TEN_B_C_AT_Light",
+			"TEN_B_C_Marksman_Light",
+			"TEN_B_C_Officer_Light",
+			"TEN_B_C_SL_Light",
+			"TEN_B_C_Rifleman_NVG",
+			"TEN_B_C_Medic_NVG",
+			"TEN_B_C_Engineer_NVG",
+			"TEN_B_C_MG_NVG",
+			"TEN_B_C_AT_NVG",
+			"TEN_B_C_Marksman_NVG",
+			"TEN_B_C_SL_NVG",
+			"TEN_B_C_Rifleman_Light_NVG",
+			"TEN_B_C_Medic_Light_NVG",
+			"TEN_B_C_Engineer_Light_NVG",
+			"TEN_B_C_MG_Light_NVG",
+			"TEN_B_C_AT_Light_NVG",
+			"TEN_B_C_Marksman_Light_NVG",
+			"TEN_B_C_SL_Light_NVG",
+			"TEN_B_C_Miguel_Blanco",
+			"TEN_B_C_Javier_Blanco",
+			"TEN_B_C_Diver_Light_NVG",
+			"TEN_B_C_Backpack",
+			"TEN_B_C_Backpack_Medic",
+			"TEN_B_C_Backpack_Engineer",
+			"TEN_B_C_Backpack_MG",
+			"TEN_B_C_Backpack_AT",
+			"TEN_B_C_Offroad_Comms",
+			"TEN_B_C_Offroad_Covered",
+			"TEN_B_C_Offroad_MG",
+			"TEN_B_C_Offroad_AT",
+			"TEN_B_C_Offroad_MG_NVG",
+			"TEN_B_C_Offroad_AT_NVG",
+			"TEN_B_C_Van",
+			"TEN_B_C_Quad",
+			"TEN_B_C_Helicopter",
+			"TEN_B_C_Helicopter_02",
+			"TEN_B_C_Plane",
+			"TEN_B_C_Static_M2_Raised",
+			"TEN_B_C_Static_M2",
+			"TEN_B_C_Static_Mortar",
+			"TEN_B_C_Boat_01",
+			"TEN_B_C_Boat_02",
+			"TEN_B_C_Boat_03",
+			"TEN_B_C_Drone_01",
+			"TEN_B_C_Drone_02",
+			"TEN_B_C_Sub_01"
+        };
     };
+};
+class CfgFactionClasses
+{
+	class TEN_B_C_Faction
+	{
+		displayName="10thMod - Blanco Cartel";
+		author="GanX";
+		icon="GX_BLANCO_CARTEL\Textures\Icons\GX_Blanco_Cartel_Icon.paa";
+		priority=2;
+		side=0;
+	};
+};
+class CfgVehicles
+{
+    #include "vehicles.hpp"
 };
 class CfgWeapons
 {
-    class GX_BC_Carrier_GL_Black;
-    class GX_BC_Carrier_GL_Black : GX_BC_Carrier_GL_Black {
-        author="GanX";
-        displayName="GX - BC Carrier GL Rig (Black)";
-        picture="\A3\Characters_F_Mark\Data\UI\icon_carrier_gl_rig_mtp.paa";
-        hiddenSelectionsTextures[]=
-        {
-            "\GX_BLANCO_CARTEL\Textures\Vests\GX_B_C_Carrier_GL_Rig_Black.paa"
-        };
-    };
-
-    class GX_BC_Carrier_Special_Black;
-    class GX_BC_Carrier_Special_Black : GX_BC_Carrier_Special_Black {
-        author="GanX";
-        displayName="GX - BC Carrier Special Rig (Black)";
-        picture="\A3\Characters_F_Mark\Data\UI\icon_carrier_spec_rig_mtp.paa";
-        hiddenSelectionsTextures[]=
-        {
-            "\GX_BLANCO_CARTEL\Textures\Vests\GX_B_C_Carrier_GL_Rig_Black.paa"
-        };
-        class ItemInfo: VestItem
-        {
-            uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02.p3d";
-            containerClass = "Supply140";
-            mass = 100;
-            class HitpointsProtectionInfo
-            {
-                class Abdomen   { hitpointName = "HitAbdomen";   armor = 16; passThrough = 0.3; };
-                class Arms      { hitpointName = "HitArms";      armor = 8;  passThrough = 0.5; };
-                class Chest     { hitpointName = "HitChest";     armor = 60; passThrough = 0.6; };
-                class Diaphragm { hitpointName = "HitDiaphragm"; armor = 60; passThrough = 0.6; };
-                class Neck      { hitpointName = "HitNeck";      armor = 8;  passThrough = 0.5; };
-                class Pelvis    { hitpointName = "HitPelvis";    armor = 16; passThrough = 0.3; };
-            };
-        };
-    };
+    #include "vests.hpp"
+};
+class CfgGroups
+{
+    #include "groups.hpp"
 };
