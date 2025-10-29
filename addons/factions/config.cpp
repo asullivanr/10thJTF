c078cc88f6234bf2c7df401ded52abf89e6e05e1
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2025-10-28 22:35:02 -0500

    factions start

---
+++ addons/factions/config.cpp
@@ -0,0 +1,20 @@
+class CfgPatches {
+    class Tenthed_factions {
+        author = "Akira";
+        name = "10th JTF: Factions";
+		requiredAddons[] = {
+			"Tenthed_core",
+		};
+        weapons[] = {};
+        units[] = {};
+    };
+};
+class CfgFactionClasses {
+    class Tenthed_UnitFact_Main2 {
+        displayname = "[10th] JTF Faction";
+        priority = 1;
+        side = 1;
+        icon = "z\10thJTF\addons\core\data\logo.paa";
+        flag = "z\10thJTF\addons\core\data\logo.paa";
+    };
+};
---
+++ addons/factions/csat_base/CfgPatches.hpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class Tenthed_faction_csat_base {
+        author = "Akira";
+        name = "10th JTF: CSAT Base faction";
+		requiredAddons[] = {
+			"Tenthed_factions",
+		};
+        weapons[] = {};
+        units[] = {};
+    };
+};
